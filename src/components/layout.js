2ec5128cb29929f85b89e26eca369b3922bb75f6
Sydney Stokes <sydneystokes@Sydneys-MacBook-Pro-5.local>
2022-04-27 15:17:24 -0400

    Added logo to navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container, heading, navbar, navLinks, navLinkItem, navLinkText, footer } from './layout.module.css'
+import { container, logo, heading, navbar, navLinks, navLinkItem, navLinkText, footer } from './layout.module.css'
+import logoImg from '../images/logo.png'
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
       <title>{pageTitle}</title>
       <nav className={navbar}>
+      <img className={logo} src={logoImg} alt="Logo"/>
         <ul className={navLinks}>
           <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
           <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
