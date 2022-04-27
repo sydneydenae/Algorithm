be679140b6736c62a79966ad128a5e48eccd84f8
Sydney Stokes <sydneystokes@Sydneys-MacBook-Pro-5.local>
2022-04-27 14:42:18 -0400

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
-import {intro} from '../components/layout.module.css'
+import {intro, mainContent} from '../components/layout.module.css'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <div className={intro}> Our custom trading algorithm makes it easy for the average person to know when to buy and sell their stocks</div>
+      <div className={intro}> Our custom trading algorithm makes it easy for the average person to know when to buy and sell their stocks!</div>
+      <div className={mainContent}>Main Content</div>
     </Layout>
   )
 }
